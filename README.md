ea2bed97d5707e4863ad4a78564bb9b170215aa1
Eric <studysavers38@gmail.com>
2025-04-17 12:09:51 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,217 @@
+# Canvas File Explorer
+
+A tool to explore a canvas course, and potentially find homework solutions and exams from previous semesters. **This does not work for every course.** This only works if a professor re-initializes a course that he/she has taught before.
+
+**Video demo**: TODO
+
+**Discord Server:** https://discord.gg/k7yNftGEAA
+
+## Why does this work?
+
+This script works because Canvas uses a sequential numbering system for file IDs. This is known as an **auto-incrementing primary key**. When a file is uploaded to Canvas, it is assigned a unique ID that is one greater than the previous file's ID. This means that the IDs are sequential and predictable. So if you know the ID of one file, you can easily find the IDs of other files by simply incrementing or decrementing the known ID.
+As an example, consider this:
+
+```
+https://canvas.example.edu/courses/123/files/455000 // Homework 10 Solutions
+https://canvas.example.edu/courses/123/files/455001 // Midterm Exam
+https://canvas.example.edu/courses/123/files/455002 // Final Exam Solutions
+
+
+https://canvas.example.edu/courses/123/files/456000 // Syllabus
+```
+
+If you have the location of the syllabus (`456000`), you can easily find the location of the other files by simply decrementing the file ID. This is what this script does. It starts at a given file ID and checks as many files as you want (specified by the `--num-files` flag) by decrementing the file ID.
+
+Our goal is to find the old files when a professor re-initializes a course. If a professor re-initializes a course, the old file IDs will appear concentrated and in close proximity due to the sequential, auto-incrementing nature of the file IDs in the database.
+
+![Sequential IDs](assets/sequential-ids.png)
+
+## Prerequisites
+
+- Install Python 3 ([Awesome Tutorial](https://realpython.com/installing-python/))
+- Open a terminal (On macOS, press `Command + Space` and type `Terminal`. On Windows, press `Windows + R` and type `cmd`)
+- Clone this repo and navigate to the directory where you cloned it. You can do this by running the following commands in your terminal:
+
+```bash
+git clone https://github.com/erict963/canvas-tool.git
+cd canvas-tool
+```
+
+This script was built with only the standard library, so no additional packages are required! You should be able to run it without any additional installations.
+
+## Usage
+
+### Example
+
+The following example command will start at `https://canvas.example.edu/courses/123/files/456000`
+and search all the URLs from `456000` to `(456000 - 10000 = 446000)`.
+
+Example command to paste into your terminal:
+
+```
+python3 canvas.py --canvas-session Ggx-OQY... \
+ --url https://canvas.example.edu/courses/123/files/456000 \
+ --num-files 10000 \
+ --use-api
+```
+
+The `--use-api` flag is optional. It will use the Canvas API to get file names instead of the default method, which is to scrape the frontend page. The API method is faster, but it may not work for all files. See the Canvas API section on why this is the case.
+
+So to be perfectly clear, the above command will check these URLs:
+
+```
+https://canvas.example.edu/api/v1/courses/123/files/456000
+https://canvas.example.edu/api/v1/courses/123/files/455999
+https://canvas.example.edu/api/v1/courses/123/files/455998
+...
+https://canvas.example.edu/api/v1/courses/123/files/446000
+```
+
+If you do not pass in the `--use-api` flag, it will resort to checking the frontend URLs instead. The URLs will look like this:
+
+```
+https://canvas.example.edu/courses/123/files/456000
+https://canvas.example.edu/courses/123/files/455999
+https://canvas.example.edu/courses/123/files/455998
+...
+https://canvas.example.edu/courses/123/files/446000
+```
+
+**Important:** The script assumes that the URL matches this ending pattern: `/files/{file_id}` where `{file_id}` is an integer.
+
+### Full Command Line Options
+
+To see all the command line options, you can run the following command:
+
+```
+python3 canvas.py -h
+```
+
+This will show you all the available options and their descriptions. Here is a summary of the options:
+
+```
+usage: canvas.py [-h] -u URL [-f NUM_FILES] [-s CANVAS_SESSION] [-w NUM_WORKERS]
+                 [-l LOG_EVERY] [--use-api]
+
+Canvas file sweeper
+
+options:
+  -h, --help            show this help message and exit
+  -u, --url URL         The URL of the file to start from, e.g.
+                        https://canvas.example.edu/courses/123/files/456
+  -f, --num-files NUM_FILES
+                        Number of files to scan (default 10000, min 1)
+  -s, --canvas-session CANVAS_SESSION
+                        The Canvas API canvas_session, provided as an environment
+                        variable or command line argument. If not provided, the
+                        script will use the CANVAS_SESSION environment variable.
+  -w, --num-workers NUM_WORKERS
+                        Number of threads to use (default 16, max 32)
+  -l, --log-every LOG_EVERY
+                        Log every (X) files found (default 1000, min 1)
+  --use-api             Experimental: Use the Canvas API instead of the frontend
+                        (default: False) - this will be faster but may not
+                        necessarily find all files. See README for more details.
+```
+
+## Canvas session
+
+The `canvas_session` is what allows this Python script to check for your Canvas files. It's like a password (hence why you **shouldn't share it with anyone**). Whenever you log into Canvas, this `canvas_session` is created and stored securely in your browser. Applications use tokens to authenticate users and authorize access to resources.
+
+In short, it gives the script permissions to act on your behalf. Don't worry, all this script is doing is checking if your professor's files are available or not. See the code in `canvas.py` for more details, and feel free to ask ChatGPT if you think any lines are suspicious.
+
+### Obtaining
+
+To obtain a `canvas_session`, first make sure you're logged into your school's Canvas. Then, visit any page in Canvas. For example, go to
+
+```
+https://canvas.example.edu/courses/123/files/456
+```
+
+Then right-click on the page and select "Inspect". This will open the developer tools. In the developer tools, select the "Application" tab.
+
+![Application Tab](assets/application-tab.png)
+
+On the left side, you should see a list of items. Click on "Cookies" and then select your school's Canvas domain. You should see a list of cookies. Look for a cookie called `canvas_session`. Copy the value of this cookie.
+
+![Obtaining Token](assets/obtaining-token.png)
+
+### Using an environment variable
+
+If you prefer to not repeatedly have to enter the `canvas_session` with the `-s` flag, you can certainly export it as an environment variable. This is done by running the following command in your terminal:
+
+```bash
+export CANVAS_SESSION=Ggx-OQY...
+```
+
+Now, you can run the script without the `--canvas-session` flag. The script will automatically use the `canvas_session` from the environment variable each time you run it.
+
+```bash
+python3 canvas.py --url https://canvas.example.edu/courses/123/files/456000 --num-files 10000 --use-api
+```
+
+## Canvas API
+
+### Why use the API?
+
+Great question! You're asking what's the difference between visiting this URL:
+
+`https://canvas.example.edu/api/v1/courses/123/files/456000` versus this URL `https://canvas.example.edu/courses/123/files/45600`
+
+Let's take a look at the api URL first. If you enter this `https://canvas.example.edu/api/v1/courses/123/files/456000` into your browser (just an example, but do try it with your courses!), you should see something like this (truncated for brevity):
+
+```json
+{
+  "id": 123,
+  "uuid": "tRuCMqv9QumS9OqBhXe2gPs0SdtUl4RHFcY5hdmo",
+   ...
+}
+```
+
+Now, let's take a look at the frontend URL. Simply delete the `api/v1/` part of the URL and enter it into your browser.
+So the URL would look like this: `https://canvas.example.edu/courses/123/files/456000`, and the response would be something like this (truncated for brevity):
+
+```html
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+  <head>
+    ...
+  </head>
+</html>
+```
+
+The main difference is the **size** of the responses. A typical example would be something like this: The API response is `300B` while the frontend response is `13kB`. This means the server will take longer to send the frontend response.
+
+### Why does the API "miss" some files (not necessarily a bad thing) ?
+
+I observed this "missing" behavior empirically, so this explanation is slightly speculative. Please let me know if you have a better explanation.
+
+If you go to the [actual code of how Canvas is implemented](https://github.com/instructure/canvas-lms/blob/ddaaa0089cb3e83783056404d44106527dfe5ef1/app/models/attachment.rb#L1756)
+
+You may see something like this:
+
+```ruby
+def destroy_content_and_replace(deleted_by_user = nil)
+```
+
+This means that when a professor re-initializes a course, the old files may start like this:
+
+```
+https://canvas.example.edu/courses/123/files/1 // Homework 10 Solutions
+https://canvas.example.edu/courses/123/files/2 // Midterm Exam
+https://canvas.example.edu/courses/123/files/3 // Final Exam Solutions
+```
+
+However, now let's say the professor makes a correction to Homework 10 Solutions before making them available to everyone. Because the content is destroyed but replaced with a new ID, and the fact that the IDs are sequential, the new file ID may look like this:
+
+```
+https://canvas.example.edu/courses/123/files/4 // Updated Homework 10 Solutions
+```
+
+So the API will no longer detect that the old file ID lives at `1`, but rather at `4`.
+
+However, the frontend will still show the old file ID `1` as a valid file, and redirect to the new file ID `4` via the API.
+
+This is why using this script to search via API may "miss" some files - the frontend relies on the API to report the correct data. Note, the frontend search will always "hit" every ID that exists.
+
+This is not necessarily a bad thing, because our entire goal is to get the "old files" that aren't currently available to the public.
---
+++ canvas.py
@@ -0,0 +1,255 @@
+import argparse
+import json
+import logging
+import os
+import re
+import time
+import urllib.request
+import urllib.error
+
+from http.cookiejar import CookieJar
+from queue import Queue
+from threading import Thread, Event
+from urllib.parse import urlparse
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s %(levelname)s] %(message)s', datefmt='%I:%M%p')
+
+def parse_response(response: urllib.response):
+    """
+    Helpers to parse the response from the Canvas API.  May be html or json.
+    """
+    if 'application/json' in response.headers.get('Content-Type'):
+        return json.loads(response.read().decode('utf-8'))
+    elif 'text/html' in response.headers.get('Content-Type'): # need to parse the html, hence why it's slower than api (bigger payloads)
+        ret = response.read().decode('utf-8')
+        display_name = re.search(r'Download (.*?)</a>', ret)
+        if not display_name:
+            raise Exception('Error parsing display name from response')
+        display_name = display_name.group(1)
+
+        path = re.search(r'<a href="([^"]*?/files/\d+)"', ret)
+        if not path:
+            raise Exception('Error parsing url from response')
+        path = path.group(1)
+        parsed = urlparse(response.geturl())
+        url = f'{parsed.scheme}://{parsed.netloc}{path}'
+        return {
+            'display_name': display_name,
+            'created_at': '',
+            'url': url + '/download?download_frd=1'
+        }   
+    else:
+        raise Exception(f'Unexpected content type: {response.headers.get("Content-Type")}')
+
+def sweep_files_reverse(start: int,
+                        increment: int, 
+                        stop: int,
+                        url: str, 
+                        **kwargs):
+    """
+    Polls url, starting from file_id = start - 1 (skipping start)
+    and decrementing by increment. Stops when it reaches file_id = stop.
+
+    For each file_id, it sends a GET request to the URL:
+        if 200 (OK), file found, add to queue. 
+        if 404 (not found), continue exploring
+        otherwise, stop the thread and return the queue.
+    
+    Example:
+    >> sweep_files_reverse(start=10, increment=2, url='https://example.com/files', stop=0)
+    would create the following threads:
+    Thread 1: will explore the odd file_ids:
+        https://example.com/files/9
+        https://example.com/files/7
+        ...
+    Thread 2: will explore the even file_ids:
+        https://example.com/files/8
+        https://example.com/files/6
+        ...
+    Both will stop when one of them reaches file_id = 0. That is,
+    Thread 1 will stop at file_id = 1
+    Thread 2 will stop at file_id = 0
+
+    By decrementing by the number of threads, we guarantee that
+    we can explore all file_ids in the range (stop, start].
+    """
+    assert start > stop, "start must be greater than stop"
+    stop_signal = Event()
+    queue = Queue()
+
+    def process(start: int, increment: int, stop: int, url: str):
+        start_time = time.time()
+        for i in range(start, stop - 1, increment):
+            if stop_signal.is_set(): return
+
+            try:
+                request = urllib.request.Request(f'{url}/{i}')
+                response = urllib.request.urlopen(request)
+                
+                response_data = parse_response(response)
+                
+                logging.info(f'FOUND: {url}/{i}')
+                
+                queue.put({
+                    'id': i,
+                    'url': f'{url}/{i}',
+                    'display_name': response_data.get('display_name'),
+                    'created_at': response_data.get('created_at'),
+                    'download_url': response_data.get('url')
+                })
+                
+            except urllib.error.HTTPError as e:
+                if e.code != 404:  # we expect majority of errors to be 404
+                    if e.code == 401: logging.error('Unauthorized access. Please get a new token.')
+                    if e.code == 403 and '(Rate Limit Exceeded)' in e.read().decode('utf-8'): logging.error('Rate limit exceeded. Please reduce number of threads.')
+                    logging.error(f'Status Code: {e.code} for {url}/{i}')
+                    # print(f'Error body: {e.read().decode("utf-8")}')
+                    stop_signal.set()
+                    return
+                if i % kwargs['log_every'] == 0:
+                    time_per_item = (time.time() - start_time) / max(start - i, 1)
+                    logging.info(f'Status: {e.code} -- File Id: {i} -- TMR: {time_per_item * (i - stop) / 60:.2f} min')
+                continue
+            except Exception as e:
+                logging.error(f'Unexpected error for File Id: {i} --> {str(e)}')
+                stop_signal.set()
+                return
+
+    threads = [ 
+        Thread(target=process, 
+                args=(
+                        start + (i * -1) - 1, # skip start
+                        increment * -1, 
+                        stop,
+                        url
+                    )) for i in range(increment) 
+    ]
+    
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    found_files = []
+    while not queue.empty():
+        found_files.append(queue.get())
+    return found_files
+
+def try_frontend(url: str):
+    try:
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
+        response_data = response.read().decode('utf-8')
+        if "Log into Canvas" in response_data:
+            logging.error('Unauthorized access. Please check your canvas session token.')
+            exit(1)
+        # print(f'Frontend URL is valid, status code: {response.status}')
+        logging.info(f'Frontend URL is valid, status code: {response.status}')
+    except urllib.error.HTTPError as e:
+        if e.code == 401:
+            logging.error('Unauthorized access. Please check your canvas session token.')
+            exit(1)
+        if e.code == 404:
+            logging.info(f'Frontend URL is valid, status code: {e.code}') # TODO: don't let 404s pass
+
+
+
+def main():
+    def validate_url(url):
+        try:
+            parsed_url = urlparse(url)
+            if not all([parsed_url.scheme, parsed_url.netloc]):
+                raise argparse.ArgumentTypeError("Invalid URL format")
+        except ValueError:
+            raise argparse.ArgumentTypeError("Invalid URL format")
+        
+        pattern = re.compile(r'files/(\d+)')
+        match = pattern.search(url)
+        if not match:
+            raise argparse.ArgumentTypeError("URL does not contain a valid file ID (e.g. /files/123)")
+        return url
+
+    env_canvas_session = os.environ.get('CANVAS_SESSION')
+
+    parser = argparse.ArgumentParser(description='Canvas file sweeper')
+    parser.add_argument('-u', '--url', 
+                        type=validate_url,
+                        required=True, 
+                        help='The URL of the file to start from, e.g. https://canvas.example.edu/courses/123/files/456')
+    parser.add_argument('-f', '--num-files', 
+                        type=lambda x: max(int(x), 1), # limit to 1 or more
+                        default=10000,
+                        help='Number of files to scan (default 10000, min 1)')
+    parser.add_argument('-s', '--canvas-session', 
+                        type=str, 
+                        required=not bool(env_canvas_session), 
+                        default=env_canvas_session,
+                        help='The Canvas API canvas session, provided as an environment variable or command line argument.  If not provided, the script will use the CANVAS_SESSION environment variable.')
+    parser.add_argument('-w', '--num-workers', 
+                        type=lambda x: min(max(int(x), 1), 32), # limit to 1-32 workers
+                        default=16, 
+                        help='Number of threads to use (default 16, max 32)')
+    parser.add_argument('-l', '--log-every', 
+                        type=lambda x: max(int(x), 1), # limit to 1 or more
+                        default=1000,  
+                        help='Log every (X) files found (default 1000, min 1)')
+    parser.add_argument('--use-api', 
+                        action='store_true', 
+                        help='Experimental: Use the Canvas API instead of the frontend (default: False) - this will be faster but may not necessarily find all files.  See README for more details.')
+    args = parser.parse_args()
+
+    # check if canvas session is provided
+    if not args.canvas_session:
+        parser.error("Canvas session token must be provided either via --canvas-session or the CANVAS_SESSION environment variable")
+
+
+    # processing the URL
+    url = args.url.rstrip('/') # remove trailing slash if present
+    if args.use_api:
+        logging.info('Using API, this may be faster but may not find all files.')
+        parsed_url = urlparse(url)
+        url = f'https://{parsed_url.netloc}/api/v1{parsed_url.path}'
+
+    start = int(re.search(r'files/(\d+)', url).group(1)) 
+    url = re.sub(r'files/\d+', '', url) # remove the file id from the URL
+    url = url.rstrip('/')
+    url = f'{url}/files'
+    
+
+    # inject the canvas canvas session 
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(CookieJar()))
+    opener.addheaders = [
+        ('Cookie', f'canvas_session={args.canvas_session}'),
+        ('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'),
+    ]
+    urllib.request.install_opener(opener)
+    
+    # print(
+    #     f'Using URL: {url}\n',
+    #     f'Using canvas session: {args.canvas_session}\n',
+    #     f'Using num files: {args.num_files}\n',
+    #     f'Using log every: {args.log_every}\n',
+    #     f'Using num workers: {args.num_workers}\n',
+    #     f'Using use_api: {args.use_api}\n'
+    # )
+    
+    try_frontend(args.url)
+        
+    results = sweep_files_reverse(
+        start=start,
+        increment=args.num_workers,
+        stop=start - args.num_files,
+        url=url,
+        log_every=args.log_every
+    )
+
+    results = sorted(results, key=lambda x: x.get('id'), reverse=True)
+    os.makedirs('output', exist_ok=True)
+    output_file = os.path.join('output', f'{time.strftime("%Y%m%d-%H%M%S")}-canvas-files.json')
+    with open(output_file, 'w') as f:
+        json.dump(results, f, indent=4)
+
+
+
+if __name__ == "__main__":
+    main()
